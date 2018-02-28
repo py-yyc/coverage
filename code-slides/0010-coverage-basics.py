c8e9198ee3c7738ccd7b954013c0d7213132c0e0
meejah <meejah@meejah.ca>
2018-02-28 16:38:09 -0700

    updates

---
+++ code-slides/0010-coverage-basics.py
@@ -0,0 +1,7 @@
+## <h1>coverage.py</h1>
+## <ul class="incremental">
+##   <li>traces every line</li>
+##   <li>universally used</li>
+##   <li>usually used with unit-tests</li>
+##   <li>(but: other use-cases)</li>
+## </ul>
---
+++ code-slides/0012-coverage-basics.py
@@ -0,0 +1,7 @@
+## <h1>coverage.py</h1>
+## <ul class="incremental">
+##   <li>running</li>
+##   <li>analysis</li>
+##   <li>reporting</li>
+##   <li> &rarr; "cuvner"</li>
+## </ul>
